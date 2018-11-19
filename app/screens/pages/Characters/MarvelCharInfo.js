244d573b873b1fc709cc16f89b5b5994eb98c160
Najib EL BERKAOUI <elberkaoui@SPIRIT.LOCAL>
2018-11-19 23:10:17 +0100

    fix and add pics to char info

--- app/screens/pages/Characters/MarvelCharInfo.js
+++ app/screens/pages/Characters/MarvelCharInfo.js
@@ -21,29 +21,31 @@ class MarvelCharInfo extends Component {
   }
   render() {
     console.log(this.props);
-    return (
-      <View>
-        <Image
-          style={{
-            borderWidth: 1,
-            borderColor: "rgba(0,0,0,0.2)",
-            alignItems: "center",
-            justifyContent: "center",
-            width: 300,
-            height: 300,
-            backgroundColor: "#fff",
-            borderRadius: 300
-          }}
-          source={{
-            uri: `${this.props.marvelChar.marvelChar.thumbnail.path}.${
-              this.props.marvelChar.marvelChar.thumbnail.extension
-            }`
-          }}
-          defaultSource={require("")}
-        />
-        <Text>{this.props.marvelChar.marvelChar.name}</Text>
-      </View>
-    );
+    if (this.props.marvelChar.marvelChar.thumbnail)
+      return (
+        <View>
+          <Image
+            style={{
+              borderWidth: 1,
+              borderColor: "rgba(0,0,0,0.2)",
+              alignItems: "center",
+              justifyContent: "center",
+              width: 300,
+              height: 300,
+              backgroundColor: "#fff",
+              borderRadius: 300
+            }}
+            source={{
+              uri: `${this.props.marvelChar.marvelChar.thumbnail.path}.${
+                this.props.marvelChar.marvelChar.thumbnail.extension
+              }`
+            }}
+          />
+          <Text>{this.props.marvelChar.marvelChar.name}</Text>
+          <Text>{this.props.marvelChar.marvelChar.description}</Text>
+        </View>
+      );
+    else return <View />;
   }
 }
 const mapStateToProps = state => {
